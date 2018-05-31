3fb9e953922bfacd29dc7f7667accbd5616bc24a
Fabian <fabian.endter@googlemail.com>
2018-05-31 20:32:44 +0200

    added the official Wetter.pdf as generator to the project

--- src/title/TitlePanel.java
+++ src/title/TitlePanel.java
@@ -84,7 +84,11 @@ public class TitlePanel extends JPanel {
         weather.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try {
+                    Desktop.getDesktop().open(new File("ressource/Wetter.pdf"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         });
     }
