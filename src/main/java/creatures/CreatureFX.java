873525fbeec894873b0e89f1e48d529651bf869b
MrEMU <fabian.endter@googlemail.com>
2019-02-28 11:37:54 +0100

    Fixed second ChoiceBox in CreatureFX, so now the first item is shown and the ChoiceBox doesnt minimized anymore Also cleaned up the CreatureFX class a bit Documentation has to follow

--- src/main/java/creatures/CreatureFX.java
+++ src/main/java/creatures/CreatureFX.java
@@ -36,6 +36,12 @@ public class CreatureFX extends Application {
     private static Scene scene;
     private ChoiceBox choiceTRegion;
     private ChoiceBox choiceRegion;
+    private TableView table;
+    private GridPane leftpanel;
+    private VBox rightpanel;
+    private Label region;
+    private Label tregion;
+    private Button go;
 
 
     /**
@@ -54,130 +60,18 @@ public class CreatureFX extends Application {
     private void drawApplication(Stage pS) {
         pS.setTitle("Kreaturenlexikon");
         root = new GridPane();
-        root.setGridLinesVisible(true);
+        //root.setGridLinesVisible(true);
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(10, 10, 10, 10));
-        root.setHgap(5.0);
+        root.setHgap(20.0);
         root.setVgap(5.0);
-        VBox leftpanel = new VBox();
-        VBox rightpanel = new VBox();
+        leftpanel = new GridPane();
+        leftpanel.setVgap(10.0);
+        rightpanel = new VBox();
 
-        Label region = new Label("Wähle Region/Sonstiges:");
-        region.setFont(new Font("Times New Roman", 18));
-        AnchorPane label1 = new AnchorPane();
-        AnchorPane.setTopAnchor(label1, 300.0);
-        AnchorPane.setLeftAnchor(label1, 500.0);
-        label1.getChildren().add(region);
-        leftpanel.getChildren().add(label1);
-
-        choiceRegion = new ChoiceBox(FXCollections.observableArrayList(
-                "Lamaran", "Nahuatlan", "Sirao", "Vesternesse", "Inseln", "Meere", "Sonstige"));
-        AnchorPane choice1 = new AnchorPane();
-        AnchorPane.setTopAnchor(choice1, 300.0);
-        AnchorPane.setLeftAnchor(choice1, 500.0);
-        choice1.getChildren().add(choiceRegion);
-        leftpanel.getChildren().add(choice1);
-
-        Label tregion = new Label("Wähle die Teilregion/Unterkategorie:");
-        tregion.setFont(new Font("Times New Roman", 18));
-        AnchorPane label2 = new AnchorPane();
-        AnchorPane.setTopAnchor(label2, 300.0);
-        AnchorPane.setLeftAnchor(label2, 500.0);
-        label2.getChildren().add(tregion);
-        leftpanel.getChildren().add(label2);
-
-        choiceTRegion = new ChoiceBox(FXCollections.observableArrayList(
-                "Buluga", "Eschar (Kairawan & Elhaddar)", "Ikenga Becken", "Küstenstaaten", "Mokattam", "Urruti"));
-        AnchorPane choice2 = new AnchorPane();
-        AnchorPane.setTopAnchor(choice2, 300.0);
-        AnchorPane.setLeftAnchor(choice2, 500.0);
-        choice2.getChildren().add(choiceTRegion);
-        leftpanel.getChildren().add(choice2);
-
-        Button go = new Button("GO");
-        AnchorPane button = new AnchorPane();
-        AnchorPane.setTopAnchor(button, 300.0);
-        AnchorPane.setLeftAnchor(button, 500.0);
-        button.getChildren().add(go);
-        leftpanel.getChildren().add(button);
-
-        CreatureRessources.init();
-        ObservableList<Creature> standard = FXCollections.observableArrayList(CreatureRessources.getFilteredBySubRegion(0, "Buluga"));
-        System.out.println(standard);
-        TableView table = new TableView(standard);
-        table.setEditable(false);
-        TableColumn<Creature,String> speciesCol = new TableColumn<Creature,String>("Spezies");
-        speciesCol.setMinWidth(200);
-        speciesCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
-                return new ReadOnlyObjectWrapper(p.getValue().getSpecies());
-            }
-        });
-        table.getColumns().add(speciesCol);
-        TableColumn<Creature,String> typeCol = new TableColumn<Creature,String>("Gattung");
-        typeCol.setMinWidth(200);
-        typeCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
-                return new ReadOnlyObjectWrapper(p.getValue().getType());
-            }
-        });
-        table.getColumns().add(typeCol);
-        TableColumn<Creature,String> habitatCol = new TableColumn<Creature,String>("Lebensraum");
-        habitatCol.setMinWidth(200);
-        habitatCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
-                return new ReadOnlyObjectWrapper(p.getValue().getHabitat());
-            }
-        });
-        table.getColumns().add(habitatCol);
-        TableColumn<Creature,String> alternativesCol = new TableColumn<Creature,String>("Alias");
-        alternativesCol.setMinWidth(200);
-        alternativesCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
-                return new ReadOnlyObjectWrapper(p.getValue().getAlternatives());
-            }
-        });
-        table.getColumns().add(alternativesCol);
-        TableColumn<Creature,String> sourceCol = new TableColumn<Creature,String>("Quelle");
-        sourceCol.setMinWidth(200);
-        sourceCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
-                return new ReadOnlyObjectWrapper(p.getValue().getSource());
-            }
-        });
-        table.getColumns().add(sourceCol);
-        rightpanel.getChildren().add(table);
-
-
-        choiceRegion.getSelectionModel().selectedIndexProperty().addListener(
-                new ChangeListener<Number>() {
-                    public void changed(ObservableValue ov,
-                        Number value, Number new_value) {
-                            setSecondChoice(new_value);
-            }
-        }
-        );
-
-        go.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-                String region = choiceRegion.getValue().toString();
-                String Tregion = choiceTRegion.getValue().toString();
-                ObservableList<String> choices = choiceRegion.getItems();
-                int tr = -1;
-                for(int i=0;i<choices.size();i++){
-                    if(choices.get(i).equals(region)){
-                        tr = i;
-                    }
-                }
-                ObservableList<Creature> newItems = FXCollections.observableArrayList(CreatureRessources.getFilteredBySubRegion(tr, Tregion));
-                table.setItems(newItems);
-            }
-        });
+        initLeftPanel();
+        initRightPanel();
+        initListeners();
 
         root.add(leftpanel,0,0);
         root.add(rightpanel,1,0);
@@ -225,6 +119,129 @@ public class CreatureFX extends Application {
                 System.out.println(temp);
 
         }
+        choiceTRegion.setValue(choiceTRegion.getItems().get(0));
+    }
+
+    private void initLeftPanel(){
+        region = new Label("Wähle Region/Sonstiges:");
+        region.setFont(new Font("Times New Roman", 18));
+        AnchorPane label1 = new AnchorPane();
+        AnchorPane.setTopAnchor(label1, 300.0);
+        AnchorPane.setLeftAnchor(label1, 500.0);
+        label1.getChildren().add(region);
+        leftpanel.add(label1,0,0);
+
+        choiceRegion = new ChoiceBox(FXCollections.observableArrayList(
+                "Lamaran", "Nahuatlan", "Sirao", "Vesternesse", "Inseln", "Meere", "Sonstige"));
+        AnchorPane choice1 = new AnchorPane();
+        AnchorPane.setTopAnchor(choice1, 300.0);
+        AnchorPane.setLeftAnchor(choice1, 500.0);
+        choice1.getChildren().add(choiceRegion);
+        leftpanel.add(choice1,0,1);
+
+        tregion = new Label("Wähle die Teilregion/Unterkategorie:");
+        tregion.setFont(new Font("Times New Roman", 18));
+        AnchorPane label2 = new AnchorPane();
+        AnchorPane.setTopAnchor(label2, 300.0);
+        AnchorPane.setLeftAnchor(label2, 500.0);
+        label2.getChildren().add(tregion);
+        leftpanel.add(label2,0,2);
+
+        choiceTRegion = new ChoiceBox(FXCollections.observableArrayList(
+                "Buluga", "Eschar (Kairawan & Elhaddar)", "Ikenga Becken", "Küstenstaaten", "Mokattam", "Urruti"));
+        AnchorPane choice2 = new AnchorPane();
+        AnchorPane.setTopAnchor(choice2, 300.0);
+        AnchorPane.setLeftAnchor(choice2, 500.0);
+        choice2.getChildren().add(choiceTRegion);
+        leftpanel.add(choice2,0,3);
+
+        go = new Button("GO");
+        AnchorPane button = new AnchorPane();
+        AnchorPane.setTopAnchor(button, 300.0);
+        AnchorPane.setLeftAnchor(button, 500.0);
+        button.getChildren().add(go);
+        leftpanel.add(button,0,4);
+    }
+
+    private void initRightPanel(){
+        CreatureRessources.init();
+        ObservableList<Creature> standard = FXCollections.observableArrayList(CreatureRessources.getFilteredBySubRegion(0, "Buluga"));
+        System.out.println(standard);
+        table = new TableView(standard);
+        table.setEditable(false);
+        TableColumn<Creature,String> speciesCol = new TableColumn<Creature,String>("Spezies");
+        speciesCol.setMinWidth(200);
+        speciesCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
+                return new ReadOnlyObjectWrapper(p.getValue().getSpecies());
+            }
+        });
+        table.getColumns().add(speciesCol);
+        TableColumn<Creature,String> typeCol = new TableColumn<Creature,String>("Gattung");
+        typeCol.setMinWidth(200);
+        typeCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
+                return new ReadOnlyObjectWrapper(p.getValue().getType());
+            }
+        });
+        table.getColumns().add(typeCol);
+        TableColumn<Creature,String> habitatCol = new TableColumn<Creature,String>("Lebensraum");
+        habitatCol.setMinWidth(200);
+        habitatCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
+                return new ReadOnlyObjectWrapper(p.getValue().getHabitat());
+            }
+        });
+        table.getColumns().add(habitatCol);
+        TableColumn<Creature,String> alternativesCol = new TableColumn<Creature,String>("Alias");
+        alternativesCol.setMinWidth(200);
+        alternativesCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
+                return new ReadOnlyObjectWrapper(p.getValue().getAlternatives());
+            }
+        });
+        table.getColumns().add(alternativesCol);
+        TableColumn<Creature,String> sourceCol = new TableColumn<Creature,String>("Quelle");
+        sourceCol.setMinWidth(200);
+        sourceCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Creature, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<Creature, String> p) {
+                return new ReadOnlyObjectWrapper(p.getValue().getSource());
+            }
+        });
+        table.getColumns().add(sourceCol);
+        rightpanel.getChildren().add(table);
+    }
+
+    private void initListeners(){
+        choiceRegion.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    public void changed(ObservableValue ov,
+                                        Number value, Number new_value) {
+                        setSecondChoice(new_value);
+                    }
+                }
+        );
+
+        go.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                String region = choiceRegion.getValue().toString();
+                String Tregion = choiceTRegion.getValue().toString();
+                ObservableList<String> choices = choiceRegion.getItems();
+                int tr = -1;
+                for(int i=0;i<choices.size();i++){
+                    if(choices.get(i).equals(region)){
+                        tr = i;
+                    }
+                }
+                ObservableList<Creature> newItems = FXCollections.observableArrayList(CreatureRessources.getFilteredBySubRegion(tr, Tregion));
+                table.setItems(newItems);
+            }
+        });
     }
 
 }
